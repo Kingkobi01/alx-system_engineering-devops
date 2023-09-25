f50fa39d6d872130dcedbe531d542f880e44af54
Kwabena Boakye <kwaboakye93@gmail.com>
2023-09-25 14:25:43 +0000

    Docs for Script

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,33 +2,45 @@
 import csv
 import requests
 from sys import argv
-"""Exports the data about the employee to a CSV file
-"""
 
 
 def main():
-    """...
-    The Main Engine
-    """
+    # Define the base API URI for the JSONPlaceholder API
+    api_uri = "https://jsonplaceholder.typicode.com"
 
-    api_uri = "https://jsonplaceholder.typicode.com"  # uri
-    user_id = argv[1]  # id of user
+    # Get the user ID from the command-line argument
+    user_id = argv[1]
 
+    # Generate a filename for the CSV file using the user ID
     filename = f"{user_id}.csv"
 
+    # Create the user URI by appending the user ID to the API URI
     user_uri = f"{api_uri}/users/{user_id}"
+
+    # Create the todos URI
     todo_uri = f"{api_uri}/todos/"
+
+    # Send a GET request to the user URI and parse the JSON response
     res = requests.get(user_uri).json()
 
-    user_name = res.get('username')  # username
+    # Extract the username from the response
+    user_name = res.get('username')
 
+    # Send a GET request to the todos URI and parse the JSON response
     res = requests.get(todo_uri).json()
 
-    user_todos = [todo for todo in res if todo['userId']
-                  == int(user_id)]
+    # Filter todos to get only those belonging to the specified user ID
+    user_todos = [todo for todo in res if todo['userId'] == int(user_id)]
 
+    # Open a CSV file with write mode using the generated filename
     with open(filename, 'w') as csv_file:
+        # Create a CSV writer
         csv_writer = csv.writer(csv_file)
+
+        # Write a header row to the CSV file
+        csv_writer.writerow(['User ID', 'Username', 'Status', 'Title'])
+
+        # Iterate through user todos and write each todo as a row in the CSV
         for todo in user_todos:
             status = todo.get('completed')
             title = todo.get('title')
@@ -36,9 +48,12 @@ def main():
 
 
 if __name__ == "__main__":
-    # Checks if the argument can be converted to an integer
     try:
+        # Check if the argument can be converted to an integer
         int(argv[1])
     except ValueError:
+        # If the argument is not a valid integer, exit the script with an error code
         exit(1)
+
+    # Call the main function
     main()
